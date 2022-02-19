5c19d0ca32a2822d75162c00da7aab20a789be04
chieu7up <chieu7up@gmail.com>
2022-02-19 20:37:59 +0700

    first commit

---
+++ app11.js
@@ -0,0 +1,12 @@
+window.onload #fuction(){
+    
+}
+function cong() {
+    so1 = document.querySelector("#so1").Vaule
+    so2 = document.querySelector("#so2").Vaule
+    tong= so1 +so2
+    document.querySelector("#result").innerHTML  "tong cua 2 so la: " +tong+ ''
+}
+function hieu(){
+    so1
+}
